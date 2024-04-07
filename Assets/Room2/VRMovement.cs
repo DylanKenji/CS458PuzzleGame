38ccc239bc97d63a971b0b6874f1d6e0754ccfdb
nzer00 <nzee4176@gmail.com>
2024-04-07 13:37:18 -0600

    Room 2 updates

---
+++ Assets/Room2/VRMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.XR;
+
+public class VRMovement : MonoBehaviour
+{
+    public XRNode inputSource; // Set this to either Left or Right controller
+    public float speed = 1.0f;
+
+    void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+
+        Vector2 inputAxis;
+        if (device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis))
+        {
+            Vector3 direction = new Vector3(inputAxis.x, 0, inputAxis.y).normalized;
+            Vector3 moveDirection = transform.TransformDirection(direction) * speed * Time.deltaTime;
+
+            // Move the controller in the calculated direction
+            transform.position += moveDirection;
+        }
+    }
+}
