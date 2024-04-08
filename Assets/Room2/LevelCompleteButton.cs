caaf66305186fc2a0a9ea8e63ffda1a6f1dd5d97
nzer00 <nzee4176@gmail.com>
2024-04-07 22:06:16 -0600

    Completed level 3 connection to main room

---
+++ Assets/Room2/LevelCompleteButton.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.SceneManagement;
+
+public class LevelCompleteButton : MonoBehaviour
+{
+    private XRGrabInteractable interactable;
+   
+    private void Start()
+    {
+        interactable = GetComponent<XRGrabInteractable>();
+        interactable.onSelectEntered.AddListener(OnGrab);
+    }
+
+    private void OnGrab(XRBaseInteractor interactor)
+    {
+        // Play destroy sound
+        RoomManager.MarkRoomCompleted(RoomManager.Room2);
+        SceneManager.LoadScene("HomeRoom");
+    }
+}
